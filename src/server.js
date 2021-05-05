6658ff769ff5d586830f567c431c551672209a56
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2021-05-05 12:50:01 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const init = async () => {
   const server = Hapi.server({
       port: 5000,
       host:
-          process.env.NODE_ENV !== "production" ? "localhost" : "172.31.43.53",
+          process.env.NODE_ENV !== "production" ? "localhost" : "172.31.37.19",
       routes: {
           cors: {
               origin: ["*"],
